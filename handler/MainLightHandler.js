d9d98044ef5e062902da8d3644728e9f345ad4af
Kasperi <kasperi@Kasperis-MacBook-Pro.local>
2021-12-08 00:04:50 +0200

    Initial app commit

---
+++ handler/MainLightHandler.js
@@ -0,0 +1,11 @@
+const DMX = require('dmx');
+
+class MainLightHandler {
+    constructor() {
+        this.handler = new DMX();
+    }
+
+
+};
+
+module.exports = MainLightHandler;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+const MainLightHandler = require('../handler/MainLightHandler');
+const dmxHandler = new MainLightHandler();
+console.log(dmxHandler);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(dmxHandler);
+  res.render('index', { title: 'Express', handlerContent: 'Tööt' });
+});
+
+module.exports = router;
